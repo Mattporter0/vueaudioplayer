f9da1020cb197156cbab57fa1e90e614193a7e1d
Mattporter0 <mattporter.contact@gmail.com>
2020-08-18 15:33:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -232,8 +232,7 @@ function mobileCss(){
 function checkifmobile(){
 	console.log("inside check if mobile");
 	if( isMobile.any() ){
-		$(".title").css("font-size", "3rem");
-		$(".artist").css("font-size", "2.5rem");
+		
 		mobileCss();
 	}
 }
